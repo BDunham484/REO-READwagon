623e1f236f3b0c68cfb3ae359330a32d626135d1
ThereWolf <bdunham484@gmail.com>
2022-07-01 17:55:50 -0500

    capture cli data

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-import inquirer from 'inquirer';
+// import inquirer from 'inquirer';
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -11,3 +11,13 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
+
+const printReadmeData = readmeDataArr => {
+    readmeDataArr.forEach((readmeItem) => console.log(readmeItem));
+};
+
+
+printReadmeData(readmeDataArgs);
